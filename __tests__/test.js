4fe8b1e6854f330992dce97ce603dd70653a7502
wmik <williemik.wmik@gmail.com>
2018-10-03 09:40:20 +0300

    test: add initial failing tests

---
+++ __tests__/test.js
@@ -0,0 +1,38 @@
+import test from "ava";
+import palebathe from "this";
+
+const palebatheTestSuite = (test, palebathe) => {
+	test("it generates an array with all the alphabets", t => {
+		const allInfo = palebathe.generateAll();
+		t.true(Array.isArray(allInfo));
+		t.is(allInfo.length, 26);
+	});
+
+	test("it returns the correct character information", t => {
+		const infoA = palebathe.getCharInfo("a");
+		t.deepEqual(infoA, t.context.constants.A);
+	});
+
+	test("imports default correctly", t => {
+		const palebatheDefault = require("this").default;
+		t.deepEqual(palebathe.getCharInfo(), palebatheDefault.getCharInfo());
+	});
+};
+
+const setup = test => {
+	test.serial("it sets up correctly", t => {
+		t.context.constants = {
+			A: {
+				upperCase: { CODE: 65, string: "A" },
+				lowerCase: { CODE: 97, string: "a" },
+				position: 1,
+				index: 0
+			}
+		};
+		t.true("constants" in t.context);
+		t.true("A" in t.context.constants);
+	});
+	return test;
+};
+
+palebatheTestSuite(setup(test), palebathe);
